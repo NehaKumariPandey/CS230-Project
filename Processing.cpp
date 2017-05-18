f821d10f381f6fca1a32390895ecf2761997e324
Pranav Sodhani <sodhanipranav@gmail.com>
2017-05-18 13:54:09 +0530

    Add clipped hashing

--- Processing.cpp
+++ Processing.cpp
@@ -4,17 +4,16 @@
 
 using namespace std;
 
-unsigned long Processing :: djb2(string str) {
+unsigned long Processing :: djb2(string str, unsigned long clip) { // set default value of clip = 1 (Clip is Optional)
     unsigned long hash = 5381;
     int c;
     int i = 0;
     while (c = str[i++])
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    return hash;
+    return hash % clip;
 }
 
-double Processing :: simscore(bitset<32> A, bitset<32> B)
-{
+double Processing :: simscore(bitset<32> A, bitset<32> B) {
 	// Given two boolean vectors, need to find jaccard distance
 	// Jaccard Distance = |A^B|/|AUB|
 	bitset<32> x = A^B;
--- Processing.h
+++ Processing.h
@@ -5,12 +5,13 @@
 #include <iostream>
 #include <bitset>
 #include <string>
+#include <climits>
 
 class Processing
 {
   public:
   double simscore(std::bitset<32>, std::bitset<32>);
-  unsigned long djb2(std::string);
+  unsigned long djb2(std::string, unsigned long clip = ULONG_MAX);
 };
 
 #endif
