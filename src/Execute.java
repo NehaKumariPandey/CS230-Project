b764eb04844a01b4bd920f982116ac1cb796385f
swati3124 <swati.arora@cs.ucla.edu>
2017-05-18 18:42:05 -0700

    K gram generation code

--- src/Execute.java
+++ src/Execute.java
@@ -1,27 +1,33 @@
-package SEProj;
 import java.util.*;
 
 /**
- * Created by swati.arora on 5/18/17.
+ * @author : pranav.sodhani, swati.arora
  */
+
 public class Execute {
 
     public static void main(String[] args){
-        Processing p1 = new Processing();
         // Testing djb2
         String stringvar = "Hello! this is a sample code.";
-        System.out.println("Hash index using djb2 hash for stringvar is " + p1.djb2(stringvar,1));
+        System.out.println("Hash index using djb2 hash for stringvar is " + Processing.djb2(stringvar,1));
 
         // Testing simiscore
-        BitSet str1 = strToBitSet("10011001100110011001100110011001");
-        BitSet str2 =  strToBitSet("10011001100110011001100110011001");
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println("Similarity score between foo and bar is " + p1.simscore(str1, str2)); // 0
-    }
+        BitSet str1 = Processing.strToBitSet("10011001100110011001100110011001");
+        BitSet str2 = Processing.strToBitSet("10011001100110011001100110011001");
+        System.out.println("Similarity score between foo and bar is " + Processing.simScore(str1, str2)); // 0
 
-    private static BitSet strToBitSet(String str) {
-        return BitSet.valueOf(new long[] { Long.parseLong(str, 2) });
+        //Testing path for files
+        System.out.println(Processing.getCompletePath());
+
+        //Testing k-grams
+        // Created four files("1.txt", "2.txt", "3.txt", "4.txt") in data/opcodes directory
+        try{
+            System.out.println(Processing.opCodeToKGrams(4,5));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 }
